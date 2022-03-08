b08f8b1373d014d707e6c2a6ea60b216c57c6efb
goschtl <ck@novareto.de>
2022-03-08 09:58:27 +0100

    changes

--- src/psyquizz/vbgtheme/__init__.py
+++ src/psyquizz/vbgtheme/__init__.py
@@ -4,6 +4,9 @@ import logging
 import uvclight
 #from os import path
 from fanstatic import Library, Resource  #, Group
+from grokcore.component import context, Subscription
+from nva.psyquizz.models.interfaces import IQuizzSecurity
+from zope.interface import implementer, Interface
 
 
 library = Library('psyquizz.vbgtheme', 'static')
@@ -13,3 +16,13 @@ vbgjs = Resource(library, 'vbg.js')
 
 def get_template(name):
     return uvclight.get_template(name, __file__)
+
+
+@implementer(IQuizzSecurity)
+class SecurityCheck(Subscription):
+    context(Interface)
+
+    def check(self, name, quizz, context):
+        if name == 'quizz3' or name == 'quizz2':
+            return False
+        return True
